1d8fdce1391fc350b3afcec0e7f17c17c06f4dd6
xunfeng911 <596927571@qq.com>
2018-01-25 17:03:50 +0800

    ad

--- source/_posts/WebGL-study.md
+++
@@ -1,7 +0,0 @@
----
-title: WebGL-study
-date: 2017-11-19 16:27:17
-tags:
----
-<!--more-->
-
--- test.js
+++ test.js
@@ -1,39 +1,11 @@
-// function lengthOfLongestSubstring(s) {
-//     const map = {};
-//     var left = 0;
-    
-//     return s.split('').reduce((max, v, i) => {
-//         left = map[v] >= left ? map[v] + 1 : left;
-//         map[v] = i;
-//         return Math.max(max, i - left + 1);
-//     }, 0);
-// }
-
-// let a = lengthOfLongestSubstring('pwwkew')
-// console.log(lengthOfLongestSubstring('pwwkew'))
-
-// var numbers = [15.5, 2.3, 1.1, 4.7];
-// function getSum(total, num) {
-//     return total + num;
-// }
-// console.log(numbers.reduce(getSum))
-
-
-var findMedianSortedArrays = function(nums1= [], nums2 = []) {
-    let b = nums1.concat(nums2).sort(sortNum), res = 0;
-    console.log(b)
-    let is = b.length % 2;
-    if (is === 0) {
-        res = (b[len - 1] + b[len]) / 2;
-    } else {
-        res = b[parseInt(len)]
+function Jsonp(src) {
+    function getData(data) {
+      return data;
     }
-    return res;
-};
-function sortNum(a, b) {
-    return a - b;
+    _src = src + '?callback=getData';
+    var _script = document.createElement("script");
+    _script.src = _src;
+    document.body.appendChild(_script);
 }
-let nums1 = [1]
-let nums2 = [2,3,4,5,6,7,8,9,10]
-
-console.log(findMedianSortedArrays(nums1, nums2));
+Jsonp('localhost:3040/?username=aa')
+console.log( Jsonp('https://api.weibo.com/2/statuses/home_timeline.json'))
