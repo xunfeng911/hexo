1de2f6617326ec4d3e51bfbdb269584462de5af5
xunfeng911 <596927571@qq.com>
2018-01-25 23:58:46 +0800

    ad

--- test.js
+++ test.js
@@ -1,11 +1,46 @@
-function Jsonp(src) {
-    function getData(data) {
-      return data;
+function A(a){
+    this.varA = a;
+  }
+  
+  // 以上函数 A 的定义中，既然 A.prototype.varA 总是会被 this.varA 遮蔽，
+  // 那么将 varA 加入到原型（prototype）中的目的是什么？
+  A.prototype = {
+    varA : null,  
+  /*
+  既然它没有任何作用，干嘛不将 varA 从原型（prototype）去掉 ? 
+  也许作为一种在隐藏类中优化分配空间的考虑 ?
+  https://developers.google.com/speed/articles/optimizing-javascript 
+  如果varA并不是在每个实例中都被初始化，那这样做将是有效果的。
+  */
+    doSomething : function(){
+      // ...
+      console.log('a')  
     }
-    _src = src + '?callback=getData';
-    var _script = document.createElement("script");
-    _script.src = _src;
-    document.body.appendChild(_script);
-}
-Jsonp('localhost:3040/?username=aa')
-console.log( Jsonp('https://api.weibo.com/2/statuses/home_timeline.json'))
+  }
+  
+  function B(a, b){
+    A.call(this, a);
+    this.varB = b;
+  }
+  B.prototype = Object.create(A.prototype, {
+    varB : {
+      value: null, 
+      enumerable: true, 
+      configurable: true, 
+      writable: true 
+    },
+    doSomething : { 
+      value: function(){ // override
+        A.prototype.doSomething.apply(this, arguments); 
+        // call super
+        // ...
+      },
+      enumerable: true,
+      configurable: true, 
+      writable: true
+    }
+  });
+  B.prototype.constructor = B;
+  
+  var b = new B('a','b');
+b.doSomething();
