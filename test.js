a79d24e0d481f4e330b787bc3df82c42c95c620e
xunfeng911 <596927571@qq.com>
2018-02-01 23:50:45 +0800

    修改结构，写博客

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
-var dog = { name: "doge" };
-var mydog = new dog()
+var obj = {};
+Object.defineProperty(ojb, "hello", {
+  get: function (val) {
+    document.getElementById('input').value = val;
+    document.getElementById('span').innerHTML = val;
+  }
+})
+
+document.addEventListener('keyup', function (e) {
+  obj.hello = e.target.value;
+})
